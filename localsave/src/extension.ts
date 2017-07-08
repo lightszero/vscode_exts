70188f36ad54af61a27ac8529aed69565998948e
lights li <lightsever@hotmail.com>
2017-07-08 16:19:22 +0800

    未完成，不知道nodejs 多个文件怎么编译。。。

--- localsave/src/extension.ts
+++ localsave/src/extension.ts
@@ -1,11 +1,13 @@
 'use strict';
+///<reference path="server.ts"/>
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import * as http from "http";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext)
+{
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
@@ -14,7 +16,8 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.sayHello', () =>
+    {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
@@ -22,8 +25,53 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
+
+    if (global["_localsave_"] == null)
+    {
+        initServer();
+    }
+}
+function initServer()
+{
+    global["_server"] =new localsave.Server();
+    var server = http.createServer((req, response) =>
+    {
+        if (req.method == "GET")
+        {
+            response.writeHead(200,
+                {
+                    'Content-Type': 'text/plain'
+                }
+            );
+
+            response.write(
+                "hello world. get" 
+                +
+                req.url
+                );
+            response.end();
+
+        }
+        else
+        {
+
+            response.writeHead(200,
+                {
+                    'Content-Type': 'text/plain'
+                }
+            );
+
+            response.write(
+
+                "hello world. POST");
+            response.end();
+        }
+    })
+    global["_server"] = server;
+    server.listen(8881);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
+export function deactivate()
+{
 }
---
+++ localsave/src/server.ts
@@ -0,0 +1,11 @@
+import * as http from "http";
+export namespace localsave
+{
+    export class Server
+    {
+        constructor(port: number)
+        {
+
+        }
+    }
+}
