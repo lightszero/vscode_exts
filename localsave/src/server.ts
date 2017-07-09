1b0e82b99fcbf95f018c197b98f3a499d6902f61
lights li <lightsever@hotmail.com>
2017-07-09 15:58:53 +0800

    abb

--- localsave/src/server.ts
+++ localsave/src/server.ts
@@ -1,5 +1,6 @@
 import * as http from "http";
 import * as fs from "fs";
+import * as path from "path";
 import * as url from "url";
 
 var IncomingForm = require("./imp/incoming_form").IncomingForm;
@@ -11,52 +12,169 @@ export namespace localsave
         {
             var server = http.createServer((req, response) =>
             {
-                if (req.method == "GET")
-                {
-                    this.onGet(req, response);
-
-                }
-                else
-                {
-                    this.onPost(req, response);
-                }
+                this.onRequest(req, response);
             })
             global["_server"] = server;
             server.listen(8881);
         }
-        onGet(req: http.IncomingMessage, response: http.ServerResponse)
+        onRequest(req: http.IncomingMessage, response: http.ServerResponse)
         {
-            response.writeHead(200,
-                {
-                    'Content-Type': 'text/plain'
-                }
-            );
-
-            response.write(
-                "hello world. get"
-                +
-                req.url
-            );
-            response.end();
-
-        }
-        onPost(req: http.IncomingMessage, response: http.ServerResponse)
-        {
-            var u = url.parse(req.url, true);
-            var callname = u.pathname;
-            var filepath: string = u.query["path"];
-            if (callname != "/savefile")
+            var _url = url.parse(req.url, true);
+            var callname = _url.pathname;
+            if (callname == "/savefile" && req.method == "POST")
+            {
+                this.onPost_SaveFile(_url, req, response);
+            }
+            else if (callname == "/deletefile")
+            {
+                this.on_DeleteFile(_url, req, response);
+            }
+            else if (callname == "/listfiles")
+            {
+                this.on_ListFiles(_url, req, response);
+            }
+            else
             {
                 response.writeHead(200,
                     {
-                        'Content-Type': 'text/plain'
+                        'Content-Type': 'text/plain;charset=UTF-8'
+                    }
+                );
+                var outjson = {
+                    tag: 0,
+                    msg: "not support method" + callname,
+                    usese:
+                    [
+                        "deletefile:localhost:8881/deletefile?path=d:\\abc.7z",
+                        "listfiles:localhost:8881/listfiles?path=d:\\",
+                        "savefile:[POST a file]localhost:8881/savefile?path=d:\\abc.7z"
+                    ]
+                };
+                response.write(JSON.stringify(outjson));
+                response.end();
+            }
+        }
+        on_DeleteFile(_url: url.Url, req: http.IncomingMessage, response: http.ServerResponse)
+        {
+            var filepath: string;
+            if (_url.query != null)
+                filepath = _url.query["path"];
+            if (req.method == "POST")
+            {
+                var f = new IncomingForm(null);
+                f.parse(req, (err, ps, files) =>
+                {
+                    if (ps["path"] != null)
+                        filepath = ps["path"];
+                });
+            }
+            if (filepath == null)
+            {
+                response.writeHead(200,
+                    {
+                        'Content-Type': 'text/plain;charset=UTF-8'
                     }
                 );
 
-                response.write("{'tag':-1,'msg':'not support method.'}");
+                response.write("{'tag':-2,'msg':'not set filename.'}");
                 response.end();
                 return;
             }
+            //删除文件
+            if (fs.existsSync(filepath))
+            {
+                fs.unlinkSync(filepath);
+                response.writeHead(200,
+                    {
+                        'Content-Type': 'text/plain;charset=UTF-8'
+                    }
+                );
+
+                response.write(
+
+                    "{'tag':0,'msg':'succ'}");
+                response.end();
+            }
+            else
+            {
+                response.writeHead(200,
+                    {
+                        'Content-Type': 'text/plain;charset=UTF-8'
+                    }
+                );
+
+                response.write(
+
+                    "{'tag':-2,'msg':'file is not exists'}");
+                response.end();
+            }
+
+        }
+        on_ListFiles(_url: url.Url, req: http.IncomingMessage, response: http.ServerResponse)
+        {
+
+            var filepath: string;
+            if (_url.query != null)
+                filepath = _url.query["path"];
+            if (req.method == "POST")
+            {
+                var f = new IncomingForm(null);
+                f.parse(req, (err, ps, files) =>
+                {
+                    if (ps["path"] != null)
+                        filepath = ps["path"];
+                });
+            }
+            if (filepath == null)
+            {
+                response.writeHead(200,
+                    {
+                        'Content-Type': 'text/plain;charset=UTF-8'
+                    }
+                );
+
+                response.write("{'tag':-2,'msg':'not set filename.'}");
+                response.end();
+                return;
+            }
+
+
+            var outjson = { 'tag': 0, 'files': [], 'msg': 'succ' };
+
+            var files = fs.readdirSync(filepath);
+            files.forEach((value, index) =>
+            {
+                var curPath = path.join(filepath, value);
+                try
+                {
+                    var fstate = fs.statSync(curPath);
+                }
+                catch (err)
+                {
+                    return;
+                }
+                var isdir = fstate.isDirectory();
+                var size = 0;
+                if (isdir == false)
+                    size = fstate.size;
+                var item = { 'isdir': isdir, 'value': value, size: size };
+
+                outjson.files.push(item);
+            })
+
+            response.writeHead(200,
+                {
+                    'Content-Type': 'text/plain;charset=UTF-8'
+                }
+            );
+
+            response.write(JSON.stringify(outjson));
+
+            response.end();
+        }
+        onPost_SaveFile(_url: url.Url, req: http.IncomingMessage, response: http.ServerResponse)
+        {
+            var filepath: string = _url.query["path"];
 
             var f = new IncomingForm(null);
             f.parse(req, (err, ps, files) =>
@@ -76,9 +194,9 @@ export namespace localsave
                     response.end();
                     return;
                 }
-                if (fs.exists(filepath))
+                if (fs.existsSync(filepath))
                 {
-                    fs.unlink(filepath);
+                    fs.unlinkSync(filepath);
                 }
                 var file;
                 for (var key in files)
@@ -98,7 +216,7 @@ export namespace localsave
                     response.end();
                     return;
                 }
-                var src = fs.createReadStream(file.path + "\\" + file.name);
+                var src = fs.createReadStream(file.path);
                 var dest = fs.createWriteStream(filepath);
                 src.pipe(dest);
                 //   console.log("got:" + err);
